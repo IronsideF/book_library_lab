a4438491c9ce19e74a7429f90e1cba9e83df98bf
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-07-13 16:13:33 +0100

    view individual book functionality

--- repositories/book_repository.py
+++ repositories/book_repository.py
@@ -35,3 +35,16 @@ def delete(id):
     """
     values = [id]
     run_sql(sql, values)
+
+def select(id):
+    book = None
+    sql = """
+    SELECT * FROM books WHERE id = %s
+    """
+    values = [id]
+    results = run_sql(sql, values)
+    if results:
+        result = results[0]
+        author = author_repo.select(result['author_id'])
+        book = Book(result['title'], result['genre'], result['fiction'], author, result['id'])
+    return book
